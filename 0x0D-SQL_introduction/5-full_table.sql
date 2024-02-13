f2bcc6ca9873ac1e79a83b3ee927a336ee52f4bc
nour eddine ichchou <ichchoun@gmail.com>
2024-02-13 04:27:46 -0800

    show tables

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
---show the description of table
-SHOW CREATE TABLE first_table;
+-- show the description of table
+SHOW CREATE TABLE first_table
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
