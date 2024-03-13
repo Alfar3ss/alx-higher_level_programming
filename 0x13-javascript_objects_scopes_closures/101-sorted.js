2a47491f6c90a41e5733cd6409033ccb4536302f
nour eddine ichchou <ichchoun@gmail.com>
2024-03-13 01:27:45 +0000

    test

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const result = {};
-for (const key in dict) {
-    if (dict.hasOwnProperty(key)) {
-        const value = dict[key];
-        if (result[value]) {
-            result[value].push(key);
-        } else {
-            result[value] = [key];
-        }
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
     }
+  }
+  newDict[valsUniq[j]] = list;
 }
-
-console.log(result);
+console.log(newDict);
